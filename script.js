2f9fb43f6ab8d2803323d6b3c588777b7b04aab9
Quines Jhona <jhonav402@gmail.com>
2025-09-11 04:36:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.doctors-carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,        // default Slick dots sa ilalim ng carousel
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
